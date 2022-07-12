c0ae5c0f0f85bb694d29feca04bfe1df1112a074
KuldeepBijarniya <84268904+KuldeepBijarniya@users.noreply.github.com>
2022-07-12 10:26:51 +0530

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,208 @@
+document.getElementById("rc1p1but").onclick = function myFunction() {
+  var doc = document.getElementById("rc1p1but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+document.getElementById("r1p1but").onclick = function myFunction() {
+  var doc = document.getElementById("r1p1but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+document.getElementById("rc1p2but").onclick = function myFunction() {
+  var doc = document.getElementById("rc1p2but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+document.getElementById("r1p2but").onclick = function myFunction() {
+  var doc = document.getElementById("r1p2but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+document.getElementById("rc1p3but").onclick = function myFunction() {
+  var doc = document.getElementById("rc1p3but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+document.getElementById("r1p3but").onclick = function myFunction() {
+  var doc = document.getElementById("r1p3but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+document.getElementById("rc1p4but").onclick = function myFunction() {
+  var doc = document.getElementById("rc1p4but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+document.getElementById("rc2p1but").onclick = function myFunction() {
+  var doc = document.getElementById("rc2p1but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+document.getElementById("r2p1but").onclick = function myFunction() {
+  var doc = document.getElementById("r2p1but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+document.getElementById("rc2p2but").onclick = function myFunction() {
+  var doc = document.getElementById("rc2p2but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+document.getElementById("r2p2but").onclick = function myFunction() {
+  var doc = document.getElementById("r2p2but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+document.getElementById("rc2p3but").onclick = function myFunction() {
+  var doc = document.getElementById("rc2p3but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+document.getElementById("r2p3but").onclick = function myFunction() {
+  var doc = document.getElementById("r2p3but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+document.getElementById("rc2p4but").onclick = function myFunction() {
+  var doc = document.getElementById("rc2p4but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+document.getElementById("rc3p1but").onclick = function myFunction() {
+  var doc = document.getElementById("rc3p1but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+document.getElementById("rc3p2but").onclick = function myFunction() {
+  var doc = document.getElementById("rc3p2but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+document.getElementById("rc3p3but").onclick = function myFunction() {
+  var doc = document.getElementById("rc3p3but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+document.getElementById("rc3p4but").onclick = function myFunction() {
+  var doc = document.getElementById("rc3p4but");
+  if (doc.innerHTML=="Added✔") {
+    doc.innerHTML = "Add+";
+    doc.style.backgroundColor = "red";
+  }
+  else {
+    doc.innerHTML = "Added✔";
+    doc.style.backgroundColor = "green";
+  }
+}
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
